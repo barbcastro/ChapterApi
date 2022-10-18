ca81cf0b7e304c5ceefafc82c97a9aa23a50eb66
Barbara <barbara_castrosilva@hotmail.com>
2022-10-18 17:38:54 -0300

    atividade encontro remoto 2

---
+++ ER2/Chapter/Chapter/Contexts/ChapterContext.cs
@@ -0,0 +1,25 @@
+﻿using Chapter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.Contexts
+{
+    public class ChapterContext: DbContext
+    {
+        public ChapterContext()
+        {
+        }
+        public ChapterContext(DbContextOptions<ChapterContext> options):base(options)
+        {
+        }
+        //vamos utilizar esse método para configurar o banco de dados
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(optionsBuilder.IsConfigured)
+            {   //cada provedor tem sua sintaxe para especificação.
+                optionsBuilder.UseSqlServer("Data Source = BARBARA-CASTRO\\SQLEXPRESS; initial catalog = Chapter; Integrated Security = true");
+            }
+        }
+        //Db representa as entidades que serão utilizadas de leitura, criação, atualização
+        public DbSet<Livro> Livros { get; set; }
+    }
+}
